a52d0124975da55e1d9995f5d643379b0aab40f9
Roman Surkov <s.rb@list.ru>
2025-02-11 16:02:30 +0200

    Доьбавлена пауза между запросами к Линкедин

--- main.py
+++ main.py
@@ -224,23 +224,26 @@ def job_exists(df, job):
     return ((df['job_url'] == job['job_url']).any() | (((df['title'] == job['title']) & (df['company'] == job['company']) & (df['date'] == job['date'])).any()))
 
 def get_jobcards(config):
-    #Function to get the job cards from the search results page
+    # Function to get the job cards from the search results page
     all_jobs = []
     for k in range(0, config['rounds']):
         for query in config['search_queries']:
-            keywords = quote(query['keywords']) # URL encode the keywords
-            location = quote(query['location']) # URL encode the location
-            for i in range (0, config['pages_to_scrape']):
+            keywords = quote(query['keywords'])  # URL encode the keywords
+            location = quote(query['location'])  # URL encode the location
+            for i in range(0, config['pages_to_scrape']):
                 url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={keywords}&location={location}&f_TPR=&f_WT={query['f_WT']}&geoId=&f_TPR={config['timespan']}&start={25*i}"
                 soup = get_with_retry(url, config)
                 jobs = transform(soup)
                 all_jobs = all_jobs + jobs
                 print("Finished scraping page: ", url)
-    print ("Total job cards scraped: ", len(all_jobs))
+                
+                # Pause between requests
+                tm.sleep(config['request_pause'] / 1000)  # Convert milliseconds to seconds
+    print("Total job cards scraped: ", len(all_jobs))
     all_jobs = remove_duplicates(all_jobs, config)
-    print ("Total job cards after removing duplicates: ", len(all_jobs))
+    print("Total job cards after removing duplicates: ", len(all_jobs))
     all_jobs = remove_irrelevant_jobs(all_jobs, config)
-    print ("Total job cards after removing irrelevant jobs: ", len(all_jobs))
+    print("Total job cards after removing irrelevant jobs: ", len(all_jobs))
     return all_jobs
 
 def find_new_jobs(all_jobs, conn, config):
